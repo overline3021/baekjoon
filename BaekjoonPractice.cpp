a9cc3545e413975a2c62762c5f3cb94da9af0315
overline3021 <overline3021@gmail.com>
2022-06-28 23:40:37 +0900

    1929_but_can't_compile_on_VS

--- BaekjoonPractice.cpp
+++ BaekjoonPractice.cpp
@@ -1,26 +1,22 @@
 #include<stdio.h>
 #include<math.h>
 
-void Factorization(int num) {
-	int tmp = 0, i = 2;
-
-	for (i; i <= num; i++) {
-		if (num % i == 0) {
-			printf("%d\n", i);
-			num /= i;
-			i--;
-		}
+void Distinguish_Prime_Num(int num_m, int num_n, int tmp_array[]) {
+	for (int i = 2; i <= num_n; i++) {
+		for (int j = 2; j * i <= num_n; j++) tmp_array[j * i] = 1;
 	}
 }
 
 int main() {
-	int Input_Num = 0;
+	int Input_N = 0, Input_M = 0, Eratosthenes[1000001] = { 0, };
 
-	scanf_s("%d", &Input_Num);
+	Eratosthenes[1] = 1;
 
-	if (Input_Num == 1) return 0;
-	
-	Factorization(Input_Num);
+	scanf_s("%d %d", &Input_M, &Input_N);
+
+	Distinguish_Prime_Num(Input_M, Input_N, Eratosthenes);
+
+	for (int i = Input_M; i <= Input_N; i++) if (Eratosthenes[i] == 0) printf("%d\n", i);
 
 	return 0;
 }
