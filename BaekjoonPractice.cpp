17083e68e90f21b8116c73455808a3adc73a2e6f
overline3021 <overline3021@gmail.com>
2022-06-29 16:01:00 +0900

    failed_4948

--- BaekjoonPractice.cpp
+++ BaekjoonPractice.cpp
@@ -1,94 +1,30 @@
 #include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include<math.h>
 
-void reverse(char Num[]) {
-    char tmp[10001];
-    int count = 0;
+int Count_Prime_Num_Until_Double(int n, int web[]) {
+	int count = 0;
 
-    count = strlen(Num) - 1;
+	for (int i = n + 1; i <= 2 * n; i++) if (web[i] == 0) count++;
 
-    for (int i = 0; count >= 0; i++) {
-        tmp[i] = Num[count];
-        count--;
-    }
-
-    tmp[strlen(Num)] = '\0';
-
-    strcpy_s(Num, 10001, tmp);
-}
-
-void sum(char a[], char b[], char result[]) {
-    int count = 0, Count_A = 0, Count_B = 0;
-
-    Count_A = strlen(a);
-    Count_B = strlen(b);
-
-    result[0] = '0';
-
-    if (Count_A > Count_B) count = Count_B;
-    else count = Count_A;
-
-    for (int i = 0; i < count; i++) {
-        if (a[i] + b[i] + result[i] - 48 * 3 >= 10) result[i + 1] = '1';
-        else result[i + 1] = '0';
-
-        result[i] = (result[i] + a[i] + b[i] - 48 * 3) % 10 + 48;
-    }
-
-    if (Count_A > Count_B) {
-        for (int i = Count_B; i < Count_A; i++) {
-            if (a[i] + result[i] - 48 * 2 >= 10) result[i + 1] = '1';
-            else result[i + 1] = '0';
-
-            result[i] = (result[i] + a[i] - 48 * 2) % 10 + 48;
-        }
-        if (result[Count_A] == '1') result[Count_A + 1] = '\0';
-        else result[Count_A] = '\0';
-    }
-    else if (Count_A < Count_B) {
-        for (int i = Count_A; i < Count_B; i++) {
-            if (b[i] + result[i] - 48 * 2 >= 10) result[i + 1] = '1';
-            else result[i + 1] = '0';
-
-            result[i] = (result[i] + b[i] - 48 * 2) % 10 + 48;
-        }
-        if (result[Count_B] == '1') result[Count_B + 1] = '\0';
-        else result[Count_B] = '\0';
-    }
-    else {
-        if (result[count] == '1') result[count + 1] = '\0';
-        else result[count] = '\0';
-    }
+	return count;
 }
 
 int main() {
-    char *Large_Num_A, *Large_Num_B, *result;
+	int Input_N = 1, Eratosthenes[123457] = { 0, };
 
-    Large_Num_A = (char*)malloc(sizeof(char) * 10001);
-    Large_Num_B = (char*)malloc(sizeof(char) * 10001);
-    result = (char*)malloc(sizeof(char) * 10002);
+	Eratosthenes[1] = 1;
 
-    if (Large_Num_A == NULL || Large_Num_B == NULL || result == NULL) goto move;
+	for (int i = 2; i <= sqrt(123456); i++) {
+		for (int j = 2; j * i <= 123456; j++) Eratosthenes[i * j] = 1;
+	}
 
-    scanf_s("%s %s", Large_Num_A, 10000, Large_Num_B, 10000);
+	while (Input_N != 0) {
+		scanf_s("%d", &Input_N);
 
-    reverse(Large_Num_A);
-    reverse(Large_Num_B);
+		if (Input_N == 0) return 0;
 
-    printf("%s\n", Large_Num_A);
-    printf("%s\n", Large_Num_B);
+		printf("%d\n", Count_Prime_Num_Until_Double(Input_N, Eratosthenes));
+	}
 
-    sum(Large_Num_A, Large_Num_B, result);
-
-    reverse(result);
-
-    for (int i = 0; i < strlen(result); i++) printf("%d", result[i] - 48);
-
-move:
-    free(Large_Num_A);
-    free(Large_Num_B);
-    free(result);
-
-    return 0;
+	return 0;
 }
