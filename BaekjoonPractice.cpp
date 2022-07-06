486f777cb18e9f77a95e20db8b61f6984bab757c
overline3021 <overline3021@gmail.com>
2022-07-06 09:11:34 +0900

    10872

--- BaekjoonPractice.cpp
+++ BaekjoonPractice.cpp
@@ -1,74 +1,13 @@
 #include<stdio.h>
-#include<string.h>
-
-void reverse(char Num[]) {
-    char tmp;
-    int count = strlen(Num);
-
-    for (int i = 0; i < count / 2; i++) {
-        tmp = Num[i];
-        Num[i] = Num[count - i - 1];
-        Num[count - i - 1] = tmp;
-    }
-}
-
-void sum(char a[], char b[], char result[]) {
-    int Count_A = 0, Count_B = 0;
-
-    Count_A = strlen(a);
-    Count_B = strlen(b);
-
-    result[0] = '0';
-
-    if (Count_A > Count_B) {
-        for (int i = 0; i < Count_B; i++) {
-            if (a[i] + b[i] + result[i] - '0' * 3 >= 10) result[i + 1] = '1';
-            else result[i + 1] = '0';
-
-            result[i] = (result[i] + a[i] + b[i] - '0' * 3) % 10 + '0';
-        }
-
-        for (int i = Count_B; i < Count_A; i++) {
-            if (a[i] + result[i] - '0' * 2 >= 10) result[i + 1] = '1';
-            else result[i + 1] = '0';
-
-            result[i] = (result[i] + a[i] - '0' * 2) % 10 + '0';
-        }
-        if (result[Count_A] == '1') result[Count_A + 1] = '\0';
-        else result[Count_A] = '\0';
-    }
-    else {
-        for (int i = 0; i < Count_A; i++) {
-            if (a[i] + b[i] + result[i] - '0' * 3 >= 10) result[i + 1] = '1';
-            else result[i + 1] = '0';
-
-            result[i] = (result[i] + a[i] + b[i] - '0' * 3) % 10 + '0';
-        }
-
-        for (int i = Count_A; i < Count_B; i++) {
-            if (b[i] + result[i] - '0' * 2 >= 10) result[i + 1] = '1';
-            else result[i + 1] = '0';
-
-            result[i] = (result[i] + b[i] - '0' * 2) % 10 + '0';
-        }
-        if (result[Count_B] == '1') result[Count_B + 1] = '\0';
-        else result[Count_B] = '\0';
-    }
-}
 
 int main() {
-    char Large_Num_A[10001], Large_Num_B[10001], result[10002];
+    int n = 0, result = 1;
 
-    scanf_s("%s %s", &Large_Num_A, 10000, &Large_Num_B, 10000);
+    scanf_s("%d", &n);
 
-    reverse(Large_Num_A);
-    reverse(Large_Num_B);
+    for (int i = 1; i <= n; i++) result *= i;
 
-    sum(Large_Num_A, Large_Num_B, result);
-
-    reverse(result);
-
-    for (int i = 0; i < strlen(result); i++) printf("%d", result[i] - '0');
+    printf("%d", result);
 
     return 0;
 }
